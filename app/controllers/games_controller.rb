0ca5db04c11772f7d0c1d5b00515377bb339fb5f
Christina Meliniotou <chris.melinioti@gmail.com>
2019-08-12 20:22:27 +0200

    longest game

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,24 @@
+require "open-uri"
+
+class GamesController < ApplicationController
+  def new
+    @letters = (0...8).map { ('a'..'z').to_a[rand(26)] }
+  end
+
+  def score
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+end
